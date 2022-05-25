6682feda78c380936e9dda901f1dfd5da8fb4c60
Erfang Chen <erfangc@gmail.com>
2022-05-24 21:49:19 -0400

    wip

--- semantic_similarity.py
+++ semantic_similarity.py
@@ -1,22 +1,39 @@
+import spacy
 from sentence_transformers import SentenceTransformer, util
 
+nlp = spacy.load("en_core_web_sm")
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-# Two lists of sentences
-sentences1 = ['The cat sits outside',
-              'A man is playing guitar',
-              'The new movie is awesome']
+text = (
+    "Our operations primarily use Navisphere, our global, "
+    "multimodal transportation management system that allows customers to communicate "
+    "worldwide with parties in their supply chain across languages, currencies, and continents. "
+    "Navisphere offers sophisticated business analytics, artificial intelligence, "
+    "and data-driven tools to improve supply chain performance and meet increasing "
+    "customer demands including the following tools"
+)
+doc = nlp(text)
 
-sentences2 = ['The dog plays in the garden',
-              'A woman watches TV',
-              'The new movie is so great']
+comparisons = [sentence.__str__() for sentence in doc.sents]
+query = ['use artificial intelligence to analyze customer behavior']
 
-# Compute embedding for both lists
-embeddings1 = model.encode(sentences1, convert_to_tensor=True)
-embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+comparison_embeddings = model.encode(comparisons, convert_to_tensor=True)
+query_embeddings = model.encode(query, convert_to_tensor=True)
 
-cosine_scores = util.cos_sim(embeddings1, embeddings2)
+#
+# cos_sim
+#
+cosine_scores = util.cos_sim(
+    comparison_embeddings,
+    query_embeddings,
+)
 
-# Output the pairs with their score
-for i in range(len(sentences1)):
-    print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
+for i in range(len(comparisons)):
+    for j in range(len(query)):
+        print("{} Score: {:.4f}".format(comparisons[i], cosine_scores[i][j]))
+
+sum = 0
+for i in range(len(comparisons)):
+    sum = sum + cosine_scores[i][0]
+
+print("Average score: {:.4f}".format(sum / len(comparisons)))
